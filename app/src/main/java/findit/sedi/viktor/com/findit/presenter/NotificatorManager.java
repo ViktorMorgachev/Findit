b079dea8d29fd6f9ad07dbab26d6e7b2e4d31151
Viktor <morgachev.v.s@mail.ru>
2019-06-28 21:35:58 +0600

    Worked

--- app/src/main/java/findit/sedi/viktor/com/findit/presenter/NotificatorManager.java
+++ app/src/main/java/findit/sedi/viktor/com/findit/presenter/NotificatorManager.java
@@ -1,75 +1,56 @@
 package findit.sedi.viktor.com.findit.presenter;
 
-import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.widget.Toast;
-
+import android.support.v4.app.NotificationManagerCompat;
 
 import findit.sedi.viktor.com.findit.R;
-
-import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class NotificatorManager {
 
     private Context mContext;
-    private String place;
 
-    public NotificatorManager(String place) {
-        this.place = place;
-    }
+    private NotificationCompat.Builder mBuilder;
+    private NotificationManagerCompat mNotificationManagerCompat;
+
 
     public NotificatorManager() {
 
     }
 
-    public void notify(Context context, double distance) {
 
-        mContext = context;
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(context);
+    private void showCompatibilityNotification(String message, int icon, String CNANNEL_ID, String title) {
 
-        showCompatibilityNotification(distance, builder);
 
+        mBuilder = new NotificationCompat.Builder(mContext, CNANNEL_ID);
+        mNotificationManagerCompat = NotificationManagerCompat.from(mContext);
+
+
+        mBuilder.setSmallIcon(icon);
+
+        if (title != null)
+            mBuilder.setContentTitle(title);
+
+        if (message != null)
+            mBuilder.setContentText(message);
+
+        mBuilder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence channelName = mContext.getResources().getString(R.string.channel_name);
+            String description = mContext.getResources().getString(R.string.channel_descrioption);
+            NotificationChannel notificationChannel = new NotificationChannel(CNANNEL_ID, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription(description);
+            mBuilder.setChannelId(CNANNEL_ID);
+        }
+
+        mNotificationManagerCompat.notify(1, mBuilder.build());
 
     }
 
-    private void showCompatibilityNotification(double distance, NotificationCompat.Builder builder) {
-
-
-        if (distance <= 1000 && distance >= 500) {
-            builder.setContentTitle("Ты почти рядом");
-            builder.setContentText("Осталось: " + (int) distance + " м");
-        }
-
-
-        if (place != null || place != "") {
-            builder.setContentTitle("Ты рядом c " + place);
-            builder.setVibrate(new long[]{1000, 1000, 1000, 1000, 1000});
-            builder.setContentText("Осталось: " + (int) distance + " м");
-        }
-
-
-        if (distance <= 100 && distance >= 70) {
-            builder.setContentTitle("Дистанция почти достигнута");
-            builder.setContentText("Осталось: " + (int) distance + " м");
-        } else if (distance < 70 && distance >= 30) {
-            builder.setContentTitle("Осталось совсем чуть чуть");
-            builder.setContentText("Осталось: " + (int) distance + " м");
-        } else if (distance < 30 && distance >= 0) {
-            builder.setVibrate(new long[]{1000, 1000, 1000, 1000, 1000});
-            builder.setContentTitle("Красавчик");
-            Toast.makeText(mContext, "Красавчик", Toast.LENGTH_LONG).show();
-            builder.setContentText("Осталось: " + (int) distance + " м");
-        }
-
-        builder.setSmallIcon(R.drawable.ic_beenhere_24dp);
-        Notification notification = builder.build();
-        NotificationManager notificationManager =
-                (NotificationManager) mContext.getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(1, notification);
-
-    }
 
 }
